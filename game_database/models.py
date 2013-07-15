8e4e33b2423b20a5362078dbedfcfe96ce46a415
symbiotech <symbiotech3@gmail.com>
2013-07-16 01:58:05 +0300

    Update models.py

--- game_database/models.py
+++ game_database/models.py
@@ -18,3 +18,17 @@ class Game(models.Model):
 
 	def __str__(self):
 		return self.team_a + " vs " + self.team_b
+		
+	
+	def update(self, game):
+		self.game_type = game['game_type']
+		self.start_time = game['start_time']
+		self.team_a = game['team_a']
+		self.team_a_aux_info_1 = game['team_a_aux_1']
+		self.team_a_score = game['team_a_score']
+		self.team_b = game['team_b']
+		self.team_b_aux_info_1 = game['team_b_aux_1']
+		self.team_b_score = game['team_b_score']
+		self.is_finished = game['is_finished']
+		self.outcome = game['outcome']
+		self.save()
